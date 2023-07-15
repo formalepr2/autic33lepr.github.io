210b42410e9ce1e84c6621b50c96f1ff2013d8e6
forma <formalepr2@gmail.com>
2023-07-15 15:32:50 -0500

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAwd8wABWfBBcwAq3OPYfYtbA7ecBllsR8",
+  authDomain: "autic33lepr-c49c8.firebaseapp.com",
+  projectId: "autic33lepr-c49c8",
+  storageBucket: "autic33lepr-c49c8.appspot.com",
+  messagingSenderId: "917596666242",
+  appId: "1:917596666242:web:efcd93a1ab35960ac0779b",
+  measurementId: "G-0DZLZ05Y9S"
+    });
